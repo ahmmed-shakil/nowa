1be29916d6f89bfd58f9b4a9eac7cd7a3a498675
ahmmed-shakil <shakilahmmed252@gmail.com>
2024-06-14 11:45:54 +0600

    Fix

--- shared/layout-components/sidebar/nav.js
+++ shared/layout-components/sidebar/nav.js
@@ -2266,14 +2266,14 @@ export const MENUITEMS = [
         selected: false,
         active: false,
         children: [
-          {
-            path: `/admin/outlets/stores`,
-            type: "link",
-            active: false,
-            selected: false,
-            title: "SKU Group",
-            accessPermission: "restaurant_sku_access",
-          },
+          // {
+          //   path: `/admin/outlets/stores`,
+          //   type: "link",
+          //   active: false,
+          //   selected: false,
+          //   title: "SKU Group",
+          //   accessPermission: "restaurant_sku_access",
+          // },
           {
             path: `/admin/restaurantSKU/Category`,
             type: "link",
@@ -2425,26 +2425,26 @@ export const MENUITEMS = [
           },
         ],
       },
-      {
-        title: "Chef Requisitions",
-        accessPermission: "chef_requisition_access",
-        path: `/`,
-        icon: (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="side-menu__icon"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-          >
-            <title>alarm-light</title>
-            <path d="M6,6.9L3.87,4.78L5.28,3.37L7.4,5.5L6,6.9M13,1V4H11V1H13M20.13,4.78L18,6.9L16.6,5.5L18.72,3.37L20.13,4.78M4.5,10.5V12.5H1.5V10.5H4.5M19.5,10.5H22.5V12.5H19.5V10.5M6,20H18A2,2 0 0,1 20,22H4A2,2 0 0,1 6,20M12,5A6,6 0 0,1 18,11V19H6V11A6,6 0 0,1 12,5Z" />
-          </svg>
-        ),
-        type: "link",
-        selected: false,
-        active: false,
-      },
+      // {
+      //   title: "Chef Requisitions",
+      //   accessPermission: "chef_requisition_access",
+      //   path: `/`,
+      //   icon: (
+      //     <svg
+      //       xmlns="http://www.w3.org/2000/svg"
+      //       className="side-menu__icon"
+      //       width="24"
+      //       height="24"
+      //       viewBox="0 0 24 24"
+      //     >
+      //       <title>alarm-light</title>
+      //       <path d="M6,6.9L3.87,4.78L5.28,3.37L7.4,5.5L6,6.9M13,1V4H11V1H13M20.13,4.78L18,6.9L16.6,5.5L18.72,3.37L20.13,4.78M4.5,10.5V12.5H1.5V10.5H4.5M19.5,10.5H22.5V12.5H19.5V10.5M6,20H18A2,2 0 0,1 20,22H4A2,2 0 0,1 6,20M12,5A6,6 0 0,1 18,11V19H6V11A6,6 0 0,1 12,5Z" />
+      //     </svg>
+      //   ),
+      //   type: "link",
+      //   selected: false,
+      //   active: false,
+      // },
       {
         title: "Items Issued",
         accessPermission: "store_item_issue_access",
